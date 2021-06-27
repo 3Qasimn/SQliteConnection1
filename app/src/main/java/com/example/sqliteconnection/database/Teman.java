831b356349b0f07ca21eae483e4fc330145f4729
3qasimn <3qasimn@gmail.com>
2021-06-27 23:39:04 +0700

    Activity 7

--- app/src/main/java/com/example/sqliteconnection/database/Teman.java
+++ app/src/main/java/com/example/sqliteconnection/database/Teman.java
@@ -5,11 +5,8 @@ public class Teman {
     String nama;
     String telpon;
 
+
     public Teman() {
-
-    }
-
-    public Teman(String id, String nama, String telpon) {
         this.id = id;
         this.nama = nama;
         this.telpon = telpon;
